e3c280f9093a260bb407e38db0b6a78ea58c99bf
Michael Guillen <michaelguillen@2ndclass-10-144-74-95.public.utexas.edu>
2018-08-06 14:26:04 -0500

    added click function, gifsinfo

--- assets/javascript/giftastic.js
+++ assets/javascript/giftastic.js
@@ -17,3 +17,50 @@ $(document).ready(function() {
             $("#animalButtons").append(x);
         };
     };
+
+    $("#addAnimal").on("click", function(event) {
+        event.preventDefault();
+        var animal = $('#animal-input').val().trim();
+        topics.push(animal);
+            renderButtons();
+    
+        });
+    
+        function gifsInfo() {
+    
+            var animal = $(this).attr("data-name");
+        
+            var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=AkFwbCSaIKXeFXXJc1BOIfByIy9ai35t&limit=10";
+        
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            }).done(function(response) {
+        
+                console.log(queryURL);
+                console.log(response);
+    
+        var results = response.data;
+    
+        for (var i = 0; i < results.length; i++) {
+    
+            var animalDiv = $("<div>");
+    
+            var gifP = $("<p>").text("Rating: " + results[i].rating);
+    
+            var gifImage = $("<img>");
+    
+            gifImage.attr("src", results[i].images.fixed_height_still.url);
+    
+            gifImage.attr("data-state", "still");
+    
+            gifImage.attr("data-still", results[i].images.fixed_height_still.url);
+    
+            gifImage.attr("data-animate", results[i].images.fixed_height.url);
+    
+            gifImage.attr("class", "gif");
+            animalDiv.append(gifP);
+            animalDiv.append(gifImage);
+            $("#animals").prepend(animalDiv);
+    
+        };
